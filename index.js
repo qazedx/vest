09d2a14ac900de4be2bd789a9fc18aff421ef566
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2015-10-29 10:55:07 +0200

    ws working

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({
+    port: 8770
+  });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('somethinffg');
+});
